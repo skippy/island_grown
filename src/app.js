ca8c8dcfc8c30719cfb82bd92839f1db9925fc08
Adam Greene <adam.greene@gmail.com>
2023-01-17 18:27:38 -0800

    adding authorization webhook

--- src/app.js
+++ src/app.js
@@ -7,8 +7,9 @@ import { logger } from './logger.js'
 
 import { igBalance } from './functions/igBalance.js'
 import { igSetup } from './functions/igSetup.js'
-import { helloHttp } from './functions/helloHttp.js'
 import { whCardholderSetup } from './functions/whCardholderSetup.js'
+import { whAuthorization } from './functions/whAuthorization.js'
+
 
 
 logger.level = config.get('log_level')
@@ -25,13 +26,12 @@ app.use(bodyParser.json({
 
 app.get('/igBalance', igBalance)
 app.get('/igSetup', igSetup)
-app.get('/helloHttp', helloHttp)
-// app.use('/helloHttp', helloHttp)
-// app.use('/wh-cardholder-setup', whCardholderSetup)
 app.post('/whCardholderSetup', express.raw({type: 'application/json'}), whCardholderSetup);
+app.post('/whAuthorization', express.raw({type: 'application/json'}), whAuthorization);
+
 // NOTE: google cloud functions needs app to be exported with the various end points specified
 // but the server.js and testing logic needs app just to be exported; so declaring it twice
-export { app as index, igBalance, igSetup, helloHttp, whCardholderSetup }
+export { app as index, igBalance, igSetup, whCardholderSetup, whAuthorization }
 export { app }
 export { config }
 // export { app as index, igBalance, igSetup, helloHttp }
---
+++ src/functions/whAuthorization.js
@@ -0,0 +1,66 @@
+import config from '../config.js'
+import bodyParser from 'body-parser'
+import * as stripeUtils from '../stripe-utils.js'
+import { logger } from '../logger.js'
+
+/**
+ *
+ * @param req https://expressjs.com/en/api.html#req
+ * @param res https://expressjs.com/en/api.html#res
+ * @return {JSON} balance object
+ */
+export const whAuthorization = async (req, res) => {
+  const sig = req.headers['stripe-signature'];
+  let event;
+
+  try {
+    event = stripeUtils.stripe.webhooks.constructEvent(req.rawBody, sig, config.get('stripe_auth_webhook_secret'));
+  } catch (err) {
+    res.status(400).send(`Webhook Error: ${err.message}`);
+    return;
+  }
+
+  // Handle the event
+  switch (event.type) {
+    case 'issuing_authorization.request':
+    case 'issuing_authorization.created':
+      const issuingAuth = event.data.object;
+      const merchantData = issuingAuth.merchant_data
+      logger.debug('Merchant Data')
+      logger.debug(merchantData)
+
+      const merchantName = merchantData.name
+      const merchantNameRegEx = new RegExp(escapeRegex(merchantName), 'i');
+      const vendors = config.get('approved_vendors')
+      const foundVendor = Object.keys(vendors).find(vn => merchantNameRegEx.test(vn) )
+      const vendorVerified = foundVendor ? vendors[foundVendor].toString() === merchantData.postal_code.toString() : false
+      logger.debug(`found vendor? ${foundVendor || false} -- verified vendor? ${vendorVerified || false}`)
+      if(vendorVerified){
+        logger.info(`auth approved: ${issuingAuth.id}`)
+        const authResponse = await stripeUtils.stripe.issuing.authorizations.approve(issuingAuth.id)
+        // logger.debug(authResponse)
+
+      }else{
+        logger.info(`auth declined: ${issuingAuth.id}`)
+        const authResponse = await stripeUtils.stripe.issuing.authorizations.decline(issuingAuth.id,
+                              { metadata: {
+                                reason: "not a verified vendor",
+                                vendor_found: foundVendor || false,
+                                mapped_postal_code: vendors[foundVendor] || false
+                              }})
+        // logger.debug(authResponse)
+      }
+
+      break;
+    default:
+      logger.warn(`Unhandled event type ${event.type}`);
+  }
+  // Return a 200 response to acknowledge receipt of the event
+  res.send();
+}
+
+
+function escapeRegex(string) {
+  return string.replace(/[/\-\\^$*+?.()|[\]{}]/g, '\\$&');
+}
+
