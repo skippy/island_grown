e9a099e368f1343103cdb8bf4f27286338335e73
Adam Greene <adam.greene@gmail.com>
2023-01-30 14:18:21 -0800

    Update whTwilio.js
    fix security alert (check type before modifying body

--- src/functions/whTwilio.js
+++ src/functions/whTwilio.js
@@ -22,7 +22,7 @@ export const whTwilio = async (req, res) => {
     // TODO: can we prevent spaming?  return something to twilio to notify them to not allow frequent retries?
     return res.status(404).send('Not Found')
   }
-  const incomingMsg = req.body.Body.toLowerCase().trim()
+  const incomingMsg = (req.body.Body.constructor === String) ? req.body.Body.toLowerCase().trim() : req.body.Body
   let responseMsg = ''
   switch (incomingMsg) {
     case 'stop':
