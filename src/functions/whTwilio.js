883e0a812fbe9016031fbc0ca4ac7f678bff7e70
Adam Greene <adam.greene@gmail.com>
2023-01-31 08:19:25 -0800

    Update whTwilio.js
    redo to fix type as instanceof didn't seem to work correctly

--- src/functions/whTwilio.js
+++ src/functions/whTwilio.js
@@ -22,7 +22,7 @@ export const whTwilio = async (req, res) => {
     // TODO: can we prevent spaming?  return something to twilio to notify them to not allow frequent retries?
     return res.status(404).send('Not Found')
   }
-  const incomingMsg = (req.body.Body instanceof String) ? req.body.Body.toLowerCase().trim() : ''
+  const incomingMsg = ((typeof req.body.Body) === 'string' ) ? req.body.Body.toLowerCase().trim() : ''
   let responseMsg = ''
   switch (incomingMsg) {
     case 'stop':
