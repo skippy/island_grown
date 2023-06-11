e0a9176e1cccc92a294a70c76dc1049e098e2836
Adam Greene <adam.greene@gmail.com>
2023-06-11 10:17:37 -0700

    slow down api hits against STRIPE API
    we're hitting the stripe API too hard, so back off a bit

--- src/functions/igUpdateCardholderSpendingRules.js
+++ src/functions/igUpdateCardholderSpendingRules.js
@@ -2,7 +2,12 @@ import * as stripeUtils from '../stripe-utils.js'
 import { spendingControls } from '../spending-controls.js'
 import { logger } from '../logger.js'
 
+//FIXME: add a library like 'limiter'
+const SLEEP_DURATION_MAX = 1000; // sleep for up to 1000 milliseconds
+const SLEEP_AFTER_NUM_ITERATIONS = 10
+
 export const igUpdateCardholderSpendingRules = async (req, res) => {
+  let counter = 0
   for await (const cardholder of stripeUtils.stripe.issuing.cardholders.list()) {
     logger.debug(`checking ${cardholder.email} (${cardholder.id}) for refills`)
     const updatedSpendingData = await spendingControls.recomputeSpendingLimits(cardholder)
@@ -23,4 +28,22 @@ export const igUpdateCardholderSpendingRules = async (req, res) => {
     }
   }
   res.send()
+  counter++
+
+  // NOTE: adding sleep because we can run into stripe API rate limiting, which is 100 calls per second
+  if (counter % SLEEP_AFTER_NUM_ITERATIONS === 0) {
+    logger.debug(`Sleeping at iteration ${counter}`);
+    const sleepDuration = getRandomInt(10, SLEEP_DURATION_MAX);
+    await sleep(sleepDuration);
+  }
+
+}
+
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
